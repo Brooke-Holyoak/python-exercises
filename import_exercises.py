85d7bcebe770e45d4cfc128e4115647b117833f5
Brooke-Holyoak <brooke.holyoak24@gmail.com>
2021-07-13 16:59:51 -0500

    import exercises

---
+++ import_exercises.py
@@ -0,0 +1,11 @@
+from function_exercises import calculate_tip
+calculate_tip(80, .30)
+print (calculate_tip(80, .30))
+
+from function_exercises import is_vowel
+print (is_vowel("t"))
+
+import itertools
+
+print (list(itertools.combinations('ABCD', 2)))
+print (list(itertools.combinations('abc', [1,2,3])))
